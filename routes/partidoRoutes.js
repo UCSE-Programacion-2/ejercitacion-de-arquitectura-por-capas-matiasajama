765eb357b5a5eca7a0ade4d0d7a0cd212fb3613e
matiasajama <matiasajama@gmail.com>
2026-06-27 01:30:22 -0300

    feat(api-partidos): agrega busquedas especializadas
    Closes #4

--- routes/partidoRoutes.js
+++ routes/partidoRoutes.js
@@ -5,11 +5,17 @@ const {
   createPartido,
   updatePartido,
   deletePartido,
+  getPartidosByTorneo,
+  getPartidosByEquipo,
+  getPartidosByFecha,
 } = require('../controllers/partidoController');
 
 const router = express.Router();
 
 router.get('/', getPartidos);
+router.get('/torneo/:torneo', getPartidosByTorneo);
+router.get('/equipo/:equipo', getPartidosByEquipo);
+router.get('/fecha/:rango', getPartidosByFecha);
 router.get('/:id', getPartidoById);
 router.post('/', createPartido);
 router.put('/:id', updatePartido);
